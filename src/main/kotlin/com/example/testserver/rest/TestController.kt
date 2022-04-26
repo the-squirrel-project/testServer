a7e373139195ea9b5f674c63bdce9586c6ddf280
Michal Borkowski <michal@borkowski.pm>
2022-04-26 13:46:16 +0200

    some changes

--- src/main/kotlin/com/example/testserver/rest/TestController.kt
+++ src/main/kotlin/com/example/testserver/rest/TestController.kt
@@ -9,4 +9,43 @@ class TestController {
     @GetMapping("/api/test")
     fun test() = "test3"
 
+    @GetMapping("/api/test2")
+    fun test2() = "test3"
+
+    @GetMapping("/api/test3")
+    fun test3() = "test3"
+
+    @GetMapping("/api/test4")
+    fun test4() = "test3"
+
+    @GetMapping("/api/test5")
+    fun test5() = "test3"
+
+    @GetMapping("/api/test6")
+    fun test6() = "test3"
+
+    @GetMapping("/api/test7")
+    fun test7() = "test3"
+
+    @GetMapping("/api/test8")
+    fun test8() = "test3"
+
+    @GetMapping("/api/test9")
+    fun test9() = "test3"
+
+    @GetMapping("/api/test10")
+    fun test10() = "test3"
+
+    @GetMapping("/api/test11")
+    fun test11() = "test3"
+
+    @GetMapping("/api/test12")
+    fun test12() = "test3"
+
+    @GetMapping("/api/test13")
+    fun test13() = "test3"
+
+    @GetMapping("/api/test14")
+    fun test14() = "test3"
+
 }
